e5934b49b269c90f0c0af14834ba648ae961340f
Daniel Finneran <dan@thebsdbox.co.uk>
2012-04-19 14:14:53 +0200

    Initial Commit

---
+++ little-plot/AppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.m
+//  little-plot
+//
+//  Created by Daniel Finneran on 19/04/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ little-plot/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  little-plot
+//
+//  Created by Daniel Finneran on 19/04/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
